41dade313f030af0e1f544e95904afea18327254
Laura Olivieri <lauraolivieri@Lauras-MacBook-Pro-2.local>
2018-03-14 01:49:10 -0400

    added the modals

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
 	$(".that").on("click", function (e) {
 		e.preventDefault();
-		var photo = $(this);
-		var src = photo.attr("src");
-		var data_src = photo.attr("data-src");
-
-		photo.attr("src", data_src);
-		photo.attr("data-src", src);
-
+		console.log("keysAreWorking");
+		var clicked_pic = $(this);
+		console.log(clicked_pic);
+		var description = clicked_pic.data("description");
+		$(".modal").find(".inner").find("p").text(description);
+		$(".modal").addClass("active");
 
 	});
+	$(".close").on("click", function (e) {
+		e.preventDefault();
 
-});
+		$(".modal").removeClass("active");
+	});
+
+
+
+})
